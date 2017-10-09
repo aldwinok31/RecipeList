28e74ccd065277d76062676ab60797ce5f51b682
Aldwin Tablante <aldwinotablante@gmail.com>
2017-10-10 06:48:39 +0800

    Initial

---
+++ Desktop/MobileFinals/app/src/main/java/aldwin/tablante/com/mobilefinals/TempData.java
@@ -0,0 +1,21 @@
+package aldwin.tablante.com.mobilefinals;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+/**
+ * Created by Bobby on 09/10/2017.
+ */
+
+public class TempData extends AppCompatActivity{
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.data_wish);
+    }
+
+
+}
